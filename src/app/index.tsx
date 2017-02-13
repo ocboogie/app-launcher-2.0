96423f8b896ae92db86b1ff57bacbf028149c6d7
ocboogie <boogie@mikulec.com>
2017-02-13 04:52:00 -0800

    Added config file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,10 @@
 import * as React from 'react';
+import * as rootRequire from 'root-require';
 import {Button, ButtonTypes} from './components/button';
 import {loadGrid, Grid} from './grid';
 
-let grid2: Grid ={
-    buttons: [
-        {text: "TEST5", type: "app", value: "C:/Program Files/Sublime Text 3/sublime_text.exe"},
-        {text: "TEST6", type: "app", value: ""},
-        {text: "TEST7", type: "app", value: ""},
-        {text: "TEST8", type: "app", value: ""},
-        {text: "TEST9", type: "cmd", value: "start ."}
-    ]
-};
+// let config: Button = jsonfile.readFileSync("../config.json");
 
-let grid: Grid ={
-            buttons: [
-                {text: "TEST1", type: "app", value: "C:/Program Files/Sublime Text 3/sublime_text.exe", color: "#ffffff"},
-                {text: "TEST2", type: "app", value: ""},
-                {text: "TEST3", type: "app", value: ""},
-                {text: "TEST4", type: "app", value: ""},
-                {text: "TEST5", type: "grid", value: grid2}
-            ]
-        };
-loadGrid(grid);
+let config: Grid = rootRequire("config.json");
+
+loadGrid(config);
